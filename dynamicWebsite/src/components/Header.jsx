82f60cdd0cbb0a0605acfd193f366d42bddeced5
Abdullah4376 <muhammadabdullah78617861@gmail.com>
2024-12-29 18:28:10 +0500

    Do some changes.

--- dynamicWebsite/src/components/Header.jsx
+++ dynamicWebsite/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from './components/Logo.jsx'
+import Logo from './Logo.jsx'
 import { useNavigate } from 'react-router'
 
 
@@ -8,17 +8,23 @@ function Header() {
     return (
         <>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <header className='font-poppins bg-slate-900 text-white sticky top-0 z-50 mx-auto flex w-full items-center justify-between p-8'>
-                <div className='flex items-center space-x-2 cursor-pointer' onClick={() => navigate('/')}>
-                <Logo />
-                <p className='text-3xl font-medium'>E-Coma</p>
+            <header className='font-poppins border-b-[1px] sticky top-0 z-50 mx-auto flex w-full items-center justify-between p-8'>
+                <div className='flex items-center space-x-28'>
+                    <div className='flex items-center space-x-2 cursor-pointer' onClick={() => navigate('/')}>
+                        <Logo />
+                        <p className='text-3xl font-medium'>E-Coma</p>
+                    </div>
+                    <div className='border rounded-3xl flex items-center px-4 py-3 gap-3'>
+                        <span className='material-symbols-outlined text-slate-700'>search</span>
+                        <input type="text" placeholder='Search for...' className='text-slate-700 border-none outline-none' />
+                    </div>
                 </div>
                 
-                <ul className='flex justify-between items-center space-x-8'>
-                <li onClick={() => navigate('/home')} className='text-2xl cursor-pointer'>Home</li>
+                {/* <ul className='flex justify-between items-center space-x-8'>
+                <li onClick={() => navigate('/')} className='text-2xl cursor-pointer'>Home</li>
                 <li onClick={() => navigate('/login')} className='text-2xl cursor-pointer'>Login</li>
                 <li onClick={() => navigate('/signup')} className='text-2xl cursor-pointer'>Signup</li>
-                </ul>
+                </ul> */}
 
                 <button onClick={() => navigate('/cart')} className='material-symbols-outlined' style={{ fontSize: '35px' }}>shopping_cart</button>
                 
--- dynamicWebsite/src/main.jsx
+++ dynamicWebsite/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Home from './pages/Home.jsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 
 
@@ -9,7 +8,6 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path={`/`} element={<App />} />
-      <Route path={`/home`} element={<Home />} />
     </Routes>
   </BrowserRouter>,
 )
